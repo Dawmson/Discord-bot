54e518ac372f9b0b3d959a91554767fb1b708aa7
Dawson <140454380+Dawmson@users.noreply.github.com>
2026-03-13 14:11:08 -0400

    Change bot token in bot.py
    Updated bot token for authentication.

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import threading
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-TOKEN = "MTQ4MTkwNzc1NzAzNTU1NjkwNg.Gz36Lm.mn3buwDjx4tUyshtHnOl3khtKg2-K4patZB4Mc"
+TOKEN = "MTQ4MTkwNzc1NzAzNTU1NjkwNg.GsdXsC.bbyvz892mScOaDuksHsXTVWLIfdVsnKoHMQymU"
 POLL_CHANNEL_ID = 1479982795651551447
 EVENT_ROLE_NAME = "Guild Wars"
 POLL_DAY = 0
